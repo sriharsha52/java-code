0befbf5bafa5ddfc5433e20684994c22a852e76e
sriharsha52 <sriharshadsu@gmail.com>
2026-08-29 02:03:12 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Even{
+    public static void main(String[] args){
+        Scanner ml=new Scanner (System.in);
+        System.out.println("enter number" );
+         int a=ml.nextInt();
+         if(a%2==0){
+            System.out.println("it is even number");
+          }
+         else {
+            System.out.println("it is odd");
+         }         
+    }
+
+}
---
+++ Usn.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Usn{
+    public static void main(String[] args){
+        Scanner ml=new Scanner (System.in);
+        System.out.println("enter first number" );
+         System.out.println("enter sec number" );
+         int a=ml.nextInt();
+         int b=ml.nextInt();
+         int sum=a+b;
+         System.out.println("the sum of two num are" + sum);
+         
+    }
+
+}
